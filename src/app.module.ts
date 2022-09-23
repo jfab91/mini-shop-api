71500583a988a3cc28a7a884656e57470164bc59
fabricio.vargas.gamanza <fabricio.vargas@gamanza.com>
2022-09-23 17:23:58 -0600

    feat: create api modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { UserModule } from './user/user.module';
+import { CustomerModule } from './customer/customer.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { BrandModule } from './brand/brand.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, UserModule, CustomerModule, CategoryModule, ProductModule, BrandModule],
   controllers: [AppController],
   providers: [AppService],
 })

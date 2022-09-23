6a7a2f27f9bee06ea980bcd656886e62b19f03a9
fabricio.vargas.gamanza <fabricio.vargas@gamanza.com>
2022-09-23 17:33:28 -0600

    feat: add CRUD endpoints

--- src/brand/brand.controller.ts
+++ src/brand/brand.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  NotImplementedException,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('brand')
-export class BrandController {}
+export class BrandController {
+  @Get()
+  public list() {
+    throw new NotImplementedException();
+  }
+
+  @Get(':id')
+  public get() {
+    throw new NotImplementedException();
+  }
+
+  @Post()
+  public create() {
+    throw new NotImplementedException();
+  }
+
+  @Put(':id')
+  public update() {
+    throw new NotImplementedException();
+  }
+
+  @Delete(':id')
+  public delete() {
+    throw new NotImplementedException();
+  }
+}
